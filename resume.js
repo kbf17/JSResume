9003b734283a2e4608123ae333ce3960300fb330
InnovateBham_12 <InnovateBham_12@Admins-iMac.local>
2017-06-15 18:09:19 -0500

    functions complete

--- resume.js
+++ resume.js
@@ -3,7 +3,6 @@ var name = 'kristine fitzgerald';
 var career = 'web programmer';
 var about = 'blah blah blah about me';
 var interests = ['Video Games', 'Delicious Tea', 'Mandolin', 'Psychological Horror Anime'];
-var skills = ['Riding bikes', ]
 // var company = [' Ocatane Coffee ', ' Babalu ', ' Space Camp '];
 // var position = ['Baker ', 'Manager ', 'Camp Counselor & Resident Advisor '];
 // var description = [
@@ -22,8 +21,6 @@ for(var i = 0; i < interests.length; i++) {
     console.log(interests[i]);
 };
 //functions
-
-
 function displayPosition(position, company, description) {
    console.log(position + ' at ' + company + description);
 } ;
@@ -31,9 +28,17 @@ console.log('Jobs I\'ve had before:');
 displayPosition ('Baker', 'Ocatane Coffee', ' - I bake tasty things');
 displayPosition ('Manager', 'Babalu', ' - I told people to clean things and gave them life advice.');
 displayPosition ('Camp Counselor & Resident Advisor', 'Space Camp', ' - Someone put me in charge of children for a week at a time. They were all nerds so they thought I was cool because I was QueenNerd.');
+console.log('Skills and Things I Has:');
 
 
-
-function displaySkill() {
-    if(var = )
-}
+function displaySkill(skills, isCool) {
+    if(isCool==true)
+        console.log('* Check It: ' + skills);
+    else {
+        console.log('* ' + skills);
+    };
+};
+displaySkill('Riding Bikes', true);
+displaySkill('Speed Reading', false);
+displaySkill('Sick Dance Moves', true);
+displaySkill('Make Pie', false);
